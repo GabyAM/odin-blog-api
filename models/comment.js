459dd0c6d27ae30b7889e7d94ac49c4f930d7129
GabyAM <gdgabyam@gmail.com>
2024-02-21 13:00:48 -0300

    defines models
    these are the basic versions of the four main models that i will be using. Admin is just a type of User with posts.

---
+++ models/comment.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema(
+    {
+        user: { type: Schema.Types.ObjectId, ref: 'User' },
+        text: String,
+        comments: [this]
+    },
+    { timestamps: true }
+);
+
+const Comment = mongoose.model('Comment', commentSchema);
---
+++ models/post.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema(
+    {
+        title: String,
+        text: String,
+        comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }]
+    },
+    { timestamps: true }
+);
+
+const Post = mongoose.model('Post', postSchema);
+module.exports = Post;
