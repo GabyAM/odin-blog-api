b0c13e794d2454dd29bd231727776722ca17828d
GabyAM <gdgabyam@gmail.com>
2024-02-23 11:54:34 -0300

    implements protected comment create route
    this route requires a text value in the body and an authorization header that contains the JWT. The function first validates the comment text and the post id on the url, and if those are correct, authenticates the user with passport's JWT strategy.
    If the user is authenticated, the comment gets saved.
    (god i hate this function)

--- controllers/comment.js
+++ controllers/comment.js
@@ -1,5 +1,9 @@
 const asyncHandler = require('express-async-handler');
 const Comment = require('../models/comment');
+const { body, param, validationResult } = require('express-validator');
+const passport = require('../passport');
+const Post = require('../models/post');
+const { default: mongoose } = require('mongoose');
 
 exports.comments_list = asyncHandler(async (req, res, next) => {
     const comments = await Comment.find({}).exec();
@@ -15,6 +19,47 @@ exports.comment_detail = asyncHandler(async (req, res, next) => {
         .exec();
     res.send(comment);
 });
+
+exports.post_comment_create_post = [
+    param('id').custom(async (value) => {
+        if (!mongoose.Types.ObjectId.isValid(value)) {
+            throw new Error('invalid post id');
+        }
+        const post = await Post.findById(value).exec();
+        if (!post) {
+            throw new Error('post not found');
+        }
+    }),
+    body('text', 'comment cannot be empty').notEmpty().escape(),
+
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.status(400).send(errors);
+        }
+        next();
+    }),
+    passport.authenticate('jwt', { session: false }),
+    asyncHandler(async (req, res, next) => {
+        const comment = new Comment({
+            user: req.user._id,
+            post: req.params.id,
+            text: req.body.text
+        });
+
+        await comment.save();
+        res.status(200).send({
+            message: 'comment created',
+            comment
+        });
+    })
+];
+
+exports.post_comment_count = asyncHandler(async (req, res, next) => {
+    const commentCount = await Comment.countDocuments({ post: req.params.id });
+    res.send({ count: commentCount });
+});
+
 exports.post_comments = asyncHandler(async (req, res, next) => {
     const comments = await Comment.find(
         {
--- routes/post.js
+++ routes/post.js
@@ -7,6 +7,7 @@ const comment_controller = require('../controllers/comment');
 router.get('/:id', post_controller.post_detail);
 
 router.get('/:id/comments', comment_controller.post_comments);
+router.post('/:id/comments', comment_controller.post_comment_create_post);
 router.get('/:id/comments/count', comment_controller.post_comment_count);
 
 module.exports = router;
