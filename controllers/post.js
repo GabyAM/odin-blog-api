806efa2563937041afa6ec1eaddc5a0d17a44f15
GabyAM <gdgabyam@gmail.com>
2024-02-27 13:45:53 -0300

    implements post creation
    now users can create posts, but only if they're admins, otherwise the server will return an error.

--- controllers/post.js
+++ controllers/post.js
@@ -1,5 +1,7 @@
 const asyncHandler = require('express-async-handler');
 const Post = require('../models/post');
+const { body, validationResult } = require('express-validator');
+const passport = require('../passport');
 
 exports.posts_list = asyncHandler(async (req, res, next) => {
     const posts = await Post.find({}).sort({ createdAt: -1 }).exec();
@@ -17,6 +19,50 @@ exports.post_detail = asyncHandler(async (req, res, next) => {
         return next(error);
     }
 });
+
+exports.post_create_post = [
+    body('title')
+        .trim()
+        .isLength({ min: 10 })
+        .withMessage('title should have at least 10 characters')
+        .custom(async (value) => {
+            const post = await Post.findOne({ title: value }).exec();
+            if (post) {
+                throw new Error('Post name already in use');
+            }
+        })
+        .escape(),
+    body('summary', 'summary should not be empty').trim().notEmpty().escape(),
+    body('text', 'text should not be empty').trim().notEmpty().escape(),
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.status(400).send(errors);
+        }
+        next();
+    }),
+    passport.authenticate('jwt', { session: false }),
+    asyncHandler(async (req, res, next) => {
+        if (!req.user.is_admin) {
+            res.status(400).send(
+                'user is not authorized to perform this action'
+            );
+        }
+
+        const post = new Post({
+            title: req.body.title,
+            author: req.user._id,
+            summary: req.body.title,
+            text: req.body.text
+        });
+
+        await post.save();
+        res.status(200).send({
+            message: 'Post created',
+            post
+        });
+    })
+];
 
 exports.user_posts = asyncHandler(async (req, res, next) => {
     const posts = await Post.find(
--- routes/post.js
+++ routes/post.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const post_controller = require('../controllers/post');
 const comment_controller = require('../controllers/comment');
 
+router.post('/', post_controller.post_create_post);
 router.get('/:id', post_controller.post_detail);
 
 router.get('/:id/comments', comment_controller.post_comments);
