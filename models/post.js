93dab5b8764c69a255a2de9b9b42c86f6ae7ecbd
GabyAM <gdgabyam@gmail.com>
2024-02-28 07:45:24 -0300

    redefines Post schema
    i been thinking for some time how to implement this, even to the point of having two different schemas for published and unpublished post, but i think this might be the solution.
    When a post is not published, its string values don't have strict validation and default to certain values. Whereas when it's published, validations of type and length are applied to those fields, making them required.

--- models/post.js
+++ models/post.js
@@ -3,14 +3,51 @@ const Schema = mongoose.Schema;
 
 const postSchema = new Schema(
     {
-        author: { type: Schema.Types.ObjectId, ref: 'User' },
-        title: String,
-        summary: String,
-        text: String,
-        is_published: Boolean
+        author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+        title: { type: String },
+        summary: { type: String },
+        text: { type: String },
+        is_published: { type: Boolean, default: false }
     },
     { timestamps: true }
 );
 
+postSchema.path('title').validate(function (value) {
+    if (this.is_published) {
+        return (
+            typeof value === 'string' && value.length > 8 && value.length < 80
+        );
+    }
+    return true;
+});
+postSchema.path('summary').validate(function (value) {
+    if (this.is_published) {
+        return (
+            typeof value === 'string' && value.length > 8 && value.length < 80
+        );
+    }
+    return true;
+});
+postSchema.path('text').validate(function (value) {
+    if (this.is_published) {
+        return typeof value === 'string' && value.length > 50;
+    }
+    return true;
+});
+
+postSchema.pre('save', function (next) {
+    if (!this.is_published) {
+        if (!this.title) {
+            this.title = 'Untitled';
+        }
+        if (!this.summary) {
+            this.summary = '';
+        }
+        if (!this.text) {
+            this.text = '';
+        }
+    }
+    next();
+});
 const Post = mongoose.model('Post', postSchema);
 module.exports = Post;
