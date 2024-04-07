f875be00c9338911c8f5f15601865e0ef3aa87a5
GabyAM <gdgabyam@gmail.com>
2024-04-07 13:00:41 -0300

    updates the image upload endpoint
    this uses all the new middlewares, that together parse the form, validate the image, handle validation errors and finally upload the file.
    the function returns the url of the saved image in case of success.

--- controllers/image.js
+++ controllers/image.js
@@ -1,42 +1,16 @@
-const multer = require('multer');
-const path = require('path');
-const authenticate = require('../middleware/authentication');
+const { authenticateAdmin } = require('../middleware/authentication');
+const parseFormData = require('../middleware/parseFormData');
+const { validateImage } = require('../utilities/validation');
+const validationMiddleware = require('../middleware/validation');
+const uploadImage = require('../middleware/fileUpload');
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './public/images');
-    },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + '_' + file.originalname;
-        cb(null, Math.round(Math.random() * 1e9) + '_' + uniqueSuffix);
-    }
-});
-
-const upload = multer({
-    storage,
-    fileFilter: function (req, file, cb) {
-        const filetypes = /jpeg|jpg|png|gif|webp/;
-
-        const extname = filetypes.test(
-            path.extname(file.originalname).toLowerCase()
-        );
-        const mimetype = filetypes.test(file.mimetype);
-
-        if (extname && mimetype) {
-            cb(null, true);
-        } else {
-            req.fileValidationError = 'File is not compatible';
-            cb(null, false);
-        }
-    }
-});
 exports.upload_image = [
-    authenticate,
-    upload.single('image'),
-    function (req, res, next) {
-        if (req.fileValidationError) {
-            res.status(400).send(req.fileValidationError);
-        }
-        res.send({ url: `images/${req.file.filename}` });
+    authenticateAdmin,
+    parseFormData,
+    validateImage(),
+    validationMiddleware,
+    uploadImage,
+    (req, res, next) => {
+        res.status(200).send({ url: req.imageUrl });
     }
 ];
