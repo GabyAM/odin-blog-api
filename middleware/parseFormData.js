d5a7c407440b20a32daccd293d84bb2b37b4533d
GabyAM <gdgabyam@gmail.com>
2024-07-21 20:47:22 -0300

    validates form-data body type before parsing
    adds error that will be returned when sending a non multipart/form-data body type on an endpoint that requires it.

--- middleware/parseFormData.js
+++ middleware/parseFormData.js
@@ -1,6 +1,11 @@
 const formidable = require('formidable');
 
 const parseFormData = (req, res, next) => {
+    if (!req.headers['content-type'].startsWith('multipart/form-data')) {
+        return res.status(400).send({
+            error: 'Invalid content type, please send the data in a multipart/form-data format'
+        });
+    }
     const form = new formidable.IncomingForm({ multiples: false });
 
     form.parse(req, (err, fields, files) => {
