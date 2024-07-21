2789c9e35feda22971374cd9d87150e281915971
GabyAM <gdgabyam@gmail.com>
2024-07-21 20:50:37 -0300

    headers conflict fix on authentication middleware

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -61,20 +61,22 @@ exports.authenticateAdmin = async function (req, res, next) {
 
                 const user = await User.findById(decodedAccessToken.id);
                 if (!user) {
-                    res.status(404).send('User not found');
+                    return res.status(404).send('User not found');
                 }
                 if (user.is_banned) {
-                    res.status(403).send('Not authorized, user is banned');
+                    return res
+                        .status(403)
+                        .send('Not authorized, user is banned');
                 }
                 if (!user.is_admin) {
-                    res.status(401).send(
-                        'User is not authorized to perform this action'
-                    );
+                    return res
+                        .status(401)
+                        .send('User is not authorized to perform this action');
                 }
                 req.user = user;
                 next();
             } catch (e) {
-                res.status(401).send('Invalid access token');
+                return res.status(401).send('Invalid access token');
             }
         } catch (e) {
             res.status(401).send('Invalid refresh token');
