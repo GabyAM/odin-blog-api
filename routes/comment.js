34ed58d0b2112e9235379c56af834a26b8581d09
GabyAM <gdgabyam@gmail.com>
2024-07-21 19:56:59 -0300

    adds leftover routes

--- routes/comment.js
+++ routes/comment.js
@@ -7,4 +7,9 @@ router.get('/:id', comment_controller.comment_detail);
 
 router.post('/:id/update', comment_controller.comment_update_post);
 
+router.post('/:id/delete', comment_controller.comment_delete_post);
+
+router.get('/:id/comments', comment_controller.comment_replies);
+router.post('/:id/comments', comment_controller.comment_reply_create_post);
+
 module.exports = router;
--- routes/post.js
+++ routes/post.js
@@ -9,6 +9,7 @@ router.post('/', post_controller.post_create_post);
 router.get('/:id', post_controller.post_detail);
 
 router.post('/:id/update', post_controller.post_update_post);
+router.post('/:id/delete', post_controller.post_delete_post);
 router.post('/:id/publish', post_controller.post_publish_post);
 router.post('/:id/unpublish', post_controller.post_unpublish_post);
 router.get('/:id/comments', comment_controller.post_comments);
@@ -16,5 +17,6 @@ router.post('/:id/comments', comment_controller.post_comment_create_post);
 router.get('/:id/comments/count', comment_controller.post_comment_count);
 router.post('/:id/save', user_controller.user_save_post);
 router.post('/:id/unsave', user_controller.user_unsave_post);
+router.get('/:id/saved', user_controller.user_check_saved_post);
 
 module.exports = router;
